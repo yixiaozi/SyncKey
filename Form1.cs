75eec0453bc75e71d814f288afeda2621bcd7453
yixiaozi <wangyangwoaini@gmail.com>
2023-08-19 06:31:17 +0800

    init

---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SyncKey
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //获取Resources中到all.json文件
+            //转换成字符串
+            string all = Encoding.UTF8.GetString(Properties.Resources.all);
+            //解析all.json文件为Json
+            Json json = Newtonsoft.Json.JsonConvert.DeserializeObject<Json>(all);
+            //设置dataGridView1的数据源为json的data
+            dataGridView1.DataSource = json.data;
+        }
+        public class Json
+        {
+            public string info { get; set; }
+            public List<Key> data { get; set; }
+            public Json()
+            {
+                data = new List<Key>();
+            }
+        }
+        public class Key
+        {
+            public string key { get; set; }
+            public string cat { get; set; }
+            public string title { get; set; }
+            public string des { get; set; }
+        }
+    }
+}
---
+++ 微力同步神KEY，通用同步key/ReadMe.txt
@@ -0,0 +1,12 @@
+关于神key的说明
+
+B3ANHK2Z5CUQKJDEJZL55C25U5GETBLTFACAGMLFWSZCQPKJ3O7BDJ6
+
+请使用浏览器直接打开index.html文件即可
+
+这是一个因兴趣而诞生的key，做为微力同步的资源共享使用
+
+最新更新增加日期和分类
+更换主力在线服务器
+-------------
+此版本神Key经过重构
