a9cc8ee2122ed6f5df11ac51bd02f2e1c7350862
yixiaozi <wangyangwoaini@gmail.com>
2023-08-25 21:46:42 +0800

    fix

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,16 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(811, 3);
+            this.button1.Location = new System.Drawing.Point(961, 2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -50,37 +54,71 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 30);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToDisplayedHeaders;
             this.dataGridView1.RowTemplate.Height = 23;
-            this.dataGridView1.Size = new System.Drawing.Size(874, 610);
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dataGridView1.Size = new System.Drawing.Size(1024, 610);
             this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.RowStateChanged += new System.Windows.Forms.DataGridViewRowStateChangedEventHandler(this.dataGridView1_RowStateChanged);
             // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(12, 3);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(694, 21);
+            this.textBox1.Size = new System.Drawing.Size(860, 21);
             this.textBox1.TabIndex = 2;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(721, 1);
+            this.button2.Location = new System.Drawing.Point(878, 3);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 3;
             this.button2.Text = "搜索";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1051, 30);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(411, 21);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Enter += new System.EventHandler(this.textBox2_Enter);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(1051, 58);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(411, 155);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.Enter += new System.EventHandler(this.richTextBox1_Enter);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(1051, 228);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(411, 412);
+            this.richTextBox2.TabIndex = 6;
+            this.richTextBox2.Text = "";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1317, 687);
+            this.ClientSize = new System.Drawing.Size(1474, 652);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "SyncKey";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -93,6 +131,9 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,29 +3,68 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static SyncKey.Form1;
 
 namespace SyncKey
 {
     public partial class Form1 : Form
     {
+        public List<Key> data = new List<Key>();
         public Form1()
         {
             InitializeComponent();
+            button1_Click(null,null);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            data.Clear();
             //获取Resources中到all.json文件
             //转换成字符串
             string all = Encoding.UTF8.GetString(Properties.Resources.all);
             //解析all.json文件为Json
             Json json = Newtonsoft.Json.JsonConvert.DeserializeObject<Json>(all);
-            //设置dataGridView1的数据源为json的data
-            dataGridView1.DataSource = json.data;
+            data.AddRange(json.data);
+            //将E:\Develop\SyncKey\微力同步神KEY，通用同步key\data文件夹下载的文件解析成Jsoninfo
+            foreach(string path in Directory.GetFiles("E:\\Develop\\SyncKey\\微力同步神KEY，通用同步key\\data"))
+            {
+                JsonInfo jsonInfo = Newtonsoft.Json.JsonConvert.DeserializeObject<JsonInfo>(File.ReadAllText(path));
+                data.AddRange(jsonInfo.data);
+            }
+
+            //将"E:\Develop\SyncKey\微力同步神KEY，通用同步key\home.json"解析成List<Jsoninfo>
+            List<JsonInfo> jsonInfos = Newtonsoft.Json.JsonConvert.DeserializeObject<List<JsonInfo>>(File.ReadAllText("E:\\Develop\\SyncKey\\微力同步神KEY，通用同步key\\home.json"));
+            foreach(JsonInfo jsonInfo in jsonInfos)
+            {
+                data.AddRange(jsonInfo.data);
+            }
+            //显示data到dataGridView1中
+            //根据key去重
+            data=data.ToArray().Distinct(new KeyComparer()).ToList();
+            dataGridView1.DataSource = data.Distinct().OrderBy(m=>m.title).ToList();
+            //第一列宽度200
+            dataGridView1.Columns[0].Width = 200;   
+            dataGridView1.Columns[1].Width = 400;   
+            dataGridView1.Columns[2].Width = dataGridView1.Width - 600 - 88- dataGridView1.FirstDisplayedScrollingColumnHiddenWidth;
+
+        }
+
+        class KeyComparer : IEqualityComparer<Key>
+        {
+            public bool Equals(Key x, Key y)
+            {
+                return x.key == y.key;
+            }
+
+            public int GetHashCode(Key obj)
+            {
+                return obj.key.GetHashCode();
+            }
         }
         public class Json
         {
@@ -36,12 +75,59 @@ namespace SyncKey
                 data = new List<Key>();
             }
         }
+        public class JsonInfo
+        {
+            public info info { get; set; }
+            public List<Key> data { get; set; }
+            public JsonInfo()
+            {
+                data = new List<Key>();
+            }
+        }
+        public class info
+        {
+            //"code":"hydy","name":"华语电影","cat_num":24
+            public string code { get; set; }
+            public string name { get; set; }
+            public int cat_num { get; set; }
+        }
         public class Key
         {
             public string key { get; set; }
-            public string cat { get; set; }
+            //public string cat { get; set; }
             public string title { get; set; }
             public string des { get; set; }
+            //public string tok { get; set; }
+        }
+
+        private void dataGridView1_RowStateChanged(object sender, DataGridViewRowStateChangedEventArgs e)
+        {
+            e.Row.HeaderCell.Value = string.Format("{0}", e.Row.Index + 1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //搜索
+            dataGridView1.DataSource = data.Where(m => m.title.Contains(textBox1.Text)|| m.des.Contains(textBox1.Text)).ToList();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //获取当前行
+            Key key = dataGridView1.Rows[e.RowIndex].DataBoundItem as Key;
+            textBox2.Text = key.key;
+            richTextBox1.Text = key.title;
+            richTextBox2.Text = key.des;
+        }
+
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+            Clipboard.SetText(textBox2.Text);
+        }
+
+        private void richTextBox1_Enter(object sender, EventArgs e)
+        {
+            Clipboard.SetText(richTextBox1.Text);
         }
     }
 }
